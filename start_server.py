412cce8144fb398853b0f4267b3671adab01bc0d
Gustavo Henrique Ferreira <143434753+xferreiraghf@users.noreply.github.com>
2025-02-20 09:11:14 -0300

    Update start_server.py

--- start_server.py
+++ start_server.py
@@ -1,14 +1,12 @@
 import os
 from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
-from waitress import serve  # Ou outro servidor WSGI de sua escolha
+from waitress import serve 
 
-# Configurações do Django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ghfconverter.settings")  # Substitua 'seu_projeto' pelo nome real do seu projeto
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ghfconverter.settings") 
 
-# Configurar o WSGI do Django
 application = get_wsgi_application()
-application = WhiteNoise(application, root="staticfiles")  # Configurando o WhiteNoise para servir arquivos estáticos
+application = WhiteNoise(application, root="staticfiles") 
 
 if __name__ == "__main__":
     serve(application, host="0.0.0.0", port=8007)
